637e2ddc6e3f593f52abee366f15e254a824be35
chaeri <coguswn7@gmail.com>
2024-06-11 16:37:42 +0900

    finish. but indiana-drag-scroll is not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 // 부트스트랩 먼저, 그 다음 custom css
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MovieList from './components/MovieList.jsx';
 import MovieListHeading from './components/MovieListHeading.jsx';
 import SearchBox from './components/SearchBox.jsx';
 import ScrollContainer from 'react-indiana-drag-scroll';
-import './App.css';
 
 
 function App() {
@@ -28,12 +28,12 @@ function App() {
   };
 
 
+
   useEffect(() => {
     if (searchValue.length > 3) {
       getMovieRequest(searchValue);
     }
   }, [searchValue]);
-
 
   useEffect(() => {
     const movieFavorites = JSON.parse(localStorage.getItem('favorites'));
@@ -42,14 +42,14 @@ function App() {
     }
   }, []);
 
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem('favorites', JSON.stringify(items));
-  };
+  // const saveToLocalStorage = (items) => {
+  //   localStorage.setItem('favorites', JSON.stringify(items));
+  // };
 
   const addFavoriteMovie = (movie) => {
     const newList = [...favorites, movie];
     setFavorites(newList);
-    saveToLocalStorage(newList);
+    localStorage.setItem('favorites', JSON.stringify(newList));  // 저장소 저장
   };
 
   const removeFavoriteMovie = (movie) => {
@@ -57,30 +57,39 @@ function App() {
       (favorite) => favorite.imdbID !== movie.imdbID
     );
     setFavorites(newList);
-    saveToLocalStorage(newList);
+    // saveToLocalStorage(newList);
+    localStorage.setItem('favorites', JSON.stringify(newList));  // 저장소 저장 (saveToLocalStorage 대신)
   };
 
 
+
   return (
-    <>
-      <div className='container-fluid movie-app'>
-        <div className='row align-items-center my-4'>
-          <MovieListHeading heading='영화 검색과 선호작 등록' />
-          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-        </div>
-
-        <ScrollContainer className="row scroll-container">
-          <MovieList movies={movies} handleClick={addFavoriteMovie} addMovie={true} />
-        </ScrollContainer>
-
-        <div className='row align-items-center my-4'>
-          <MovieListHeading heading='내 선호작' />
-        </div>
-        <ScrollContainer className="row scroll-container">
-          <MovieList movies={favorites} handleClick={removeFavoriteMovie} addMovie={false} />
-        </ScrollContainer>
+    <div className='container-fluid movie-app'>
+      <div className='row align-items-center my-4'>
+        <MovieListHeading heading='영화 검색과 선호작 등록' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
-    </>
+
+      <ScrollContainer className="row scroll-container">
+        <MovieList
+          movies={movies}
+          handleClick={addFavoriteMovie}
+          addMovie={true}
+        />
+      </ScrollContainer>
+
+      <div className='row align-items-center my-4'>
+        <MovieListHeading heading='내 선호작' />
+      </div>
+
+      <ScrollContainer className="row scroll-container">
+        <MovieList
+          movies={favorites}
+          handleClick={removeFavoriteMovie}
+          addMovie={false}
+        />
+      </ScrollContainer>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// React 라이브러리 import해서 JSX 구문 사용하게 함
 import React from 'react';
+// ReactDOM 라이브러리의 새로운 루트 API를 import. React 컴포넌트를 실제 DOM에 렌더링하는 데 사용됨
 import ReactDOM from 'react-dom/client';
+// App 컴포넌트를 import. 애플리케이션의 최상위 컴포넌트(모든 하위 컴포넌트를 포함함)
 import App from './App';
 
+// 루트 생성
+// index.html 파일 내의 id가 "root"인 <div> 요소를 가져옴
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// App 컴포넌트를 렌더링 (이 컴포넌트와 그 하위 컴포넌트들이 #root 요소 내에 렌더링됨)
+// 애플리케이션을 화면에 표시함
 root.render(
   <App />
 );
